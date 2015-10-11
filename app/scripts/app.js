f5a5b04c6e14c3639a95ecac08b429e6cda4e33e
Kevin Ainley-Walker <kevinainleywalker@hotmail.com>
2015-10-11 12:59:55 +0100

    add routes and base partials for each page@

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,5 +6,10 @@ angular.module('MyApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
+	$routeProvider.when('/what', {templateUrl: 'partials/what.html', controller: 'HomeController'});
+	$routeProvider.when('/why', {templateUrl: 'partials/why.html', controller: 'HomeController'});
+	$routeProvider.when('/how', {templateUrl: 'partials/how.html', controller: 'HomeController'});
+	$routeProvider.when('/gallery', {templateUrl: 'partials/gallery.html', controller: 'HomeController'});
+	$routeProvider.when('/links', {templateUrl: 'partials/links.html', controller: 'HomeController'});
 	$routeProvider.otherwise({redirectTo: '/home'});
 }]);
